75ce4212052c93937b3070d3c95b8b1ffabb6fa2
DWizard11 <daniyalisawesome111@gmail.com>
2022-06-25 10:15:18 +0800

    Initial Commit

---
+++ To_Do_List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To_Do_List
+//
+//  Created by DWizard11 on 25/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To_Do_List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To_Do_List
+//
+//  Created by DWizard11 on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
